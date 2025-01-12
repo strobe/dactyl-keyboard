10534e1e24646a93887dfe0bbf15418d2cf019bf
Evgeniy Tokarev <archjohn@gmail.com>
2025-01-13 00:56:47 +0200

    fix nix shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.cmake
-    pkgs.cq-editor
+  buildInputs = with pkgs; [
+    cmake
+    conda
+    expat
+    libz
+    coreutils
+    binutils
+    libgcc
+    #pkgs.cq-editor
   ];
+
+  shellHook = ''
+    echo conda path ${pkgs.conda}
+    echo expat path ${pkgs.expat}
+    #export PATH=$PATH:${pkgs.conda}/bin
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.expat}/lib
+  '';
 }
